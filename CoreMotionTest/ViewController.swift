8e671f8da8b6aa66f678541b86fcde2d07c97fb7
sinyilin <sinyilin@novastargame.net>
2024-08-09 17:12:58 +0800

    Initial Commit

---
+++ CoreMotionTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMotionTest
+//
+//  Created by novastar on 2024/8/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
